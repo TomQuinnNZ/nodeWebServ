2e0c554d21910e7dfc15b6b1f58124bb9dfc83af
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-18 15:31:10 +1200

    tailor response based on request url header

--- src/app.js
+++ src/app.js
@@ -2,12 +2,34 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    const obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
-    response.end(JSON.stringify(obj));
+
+    if (request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html'});
+        // Convert file contents to UTF8 (string) so we can use string substitution in the callback
+        fs.readFile(__dirname + '/index.htm', 'utf8', (err, data) => { 
+            if (err) {
+                response.end(err);
+            }
+            else {
+                const message = 'Hello world (placeholder via template)!';
+                const html = data.replace('{Message}', message);
+                response.end(html);
+            }
+        });
+    }
+    else if (request.url === '/api') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        const obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        response.end(JSON.stringify(obj));
+    }
+    else {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Default endpoint if not an API or JSON call.');
+    }
+
 }).listen(1337, '127.0.0.1');
 
 
