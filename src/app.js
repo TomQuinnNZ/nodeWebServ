1e7f30a76303d246c0a6ad62f4bb1574b1ec84f3
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-18 12:01:45 +1200

    pipe template html to a writeStream response

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain'});
-    response.end('Hello');
+    response.writeHead(200, { 'Content-Type': 'text/html'});
+    // Convert file contents to UTF8 (string) so we can use string substitution in the callback
+    
+    var html = fs.createReadStream(__dirname + '/index.htm');
+    html.pipe(response);
+
+
+    /*     fs.readFile(__dirname + '/index.htm', 'utf8', (err, data) => {
+        if (err) {
+            response.end(err);
+        }
+        else {
+            const message = 'Hello world (placeholder via template)!';
+            const html = data.replace('{Message}', message);
+            response.end(html);
+        }
+    }); */
 }).listen(1337, '127.0.0.1');
