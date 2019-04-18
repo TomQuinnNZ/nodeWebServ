e36e719c3c2f69b7311fe91e4588585bf0959055
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-18 14:18:44 +1200

    return json to the browser instead

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    const obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    };
+    response.end(JSON.stringify(obj));
+}).listen(1337, '127.0.0.1');
+
+
+/* http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html'});
     // Convert file contents to UTF8 (string) so we can use string substitution in the callback
     
@@ -9,14 +19,14 @@ http.createServer(function(request, response) {
     html.pipe(response);
 
 
-    /*     fs.readFile(__dirname + '/index.htm', 'utf8', (err, data) => {
-        if (err) {
-            response.end(err);
-        }
-        else {
-            const message = 'Hello world (placeholder via template)!';
-            const html = data.replace('{Message}', message);
-            response.end(html);
-        }
-    }); */
-}).listen(1337, '127.0.0.1');
+    // fs.readFile(__dirname + '/index.htm', 'utf8', (err, data) => {
+    //     if (err) {
+    //         response.end(err);
+    //     }
+    //     else {
+    //         const message = 'Hello world (placeholder via template)!';
+    //         const html = data.replace('{Message}', message);
+    //         response.end(html);
+    //     }
+    // });
+}).listen(1337, '127.0.0.1'); */
