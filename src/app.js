efbd652132e19a4d16303dc0479a4576e37c04c8
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-23 11:53:20 +1200

    show how to pull out/use URI arguments

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ app.get('/', (request, response) => {
     response.send('<html><head></head><body><h1>Hello world (Express)!</h1></body></html>');
 });
 
-app.get('/api', (request, response) => {
-    response.json({ firstname: 'John', lastname: 'Doe' });
+// colon before the id tells express that 'id' could be anything
+app.get('/person/:id', (request, response) => {
+    // 'id' can then be accessed from request.params
+    response.send(`<html><head></head><body><h1>Person ID: ${request.params.id}</h1></body></html>`);
 });
 
 app.listen(port);
