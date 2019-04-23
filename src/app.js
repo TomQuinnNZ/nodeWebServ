c6a75c94a1829c56eaea4629f80e81798c6adcdb
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-23 13:54:57 +1200

    use style.css to style html, add EJS to the project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+//static files now pulled from /public directory via middleware function
+var staticfilepath = path.join(__dirname, '../public');
+app.use('/assets', express.static(staticfilepath));
+
+app.use('/', function(request, response, next) {
+    console.log(`Request URL: ${request.url}`);
+    next();
+});
+
 app.get('/', (request, response) => {
-    response.send('<html><head></head><body><h1>Hello world (Express)!</h1></body></html>');
+    response.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world (Express)!</h1></body></html>');
 });
 
 // colon before the id tells express that 'id' could be anything
